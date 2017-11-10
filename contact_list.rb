7fc18167e0140e3d3660171c46d3132f84bbbdc3
Jace P. Gold <Jace@JaceGold.com>
2017-11-10 12:24:42 -0700

    new game and contact loop

--- contact_list.rb
+++ contact_list.rb
@@ -8,11 +8,14 @@ puts '1) Show Contacts'
 puts '2) Add Contact'
 puts '3) Quit'
 
+contacts = ['Jace', 'Alec', 'Travis', 'Jana']
 user_input = gets.to_i
 
 case user_input
   when 1
-    puts 'show all contacts'
+    contacts.each do |contact|
+      puts "Contact: #{contact}"
+    end
   when 2
     puts 'add a new contact'
   when 3
